e32b655e201253048fd4b304f06fcd3084d183b8
codewithnitish18 <ethereal.nitish05@gmail.com>
2026-09-12 02:48:04 +0530

    Add portfolio contact form with EmailJS

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+emailjs.init({
+    publicKey: "BGqUb7FNESQfvwNFl"
+});
+
 const menuIcon = document.querySelector("#menu-icon");
 const navbar = document.querySelector(".navbar");
 const navLinks = document.querySelectorAll(".navbar a");
@@ -10,4 +14,25 @@ navLinks.forEach((link) => {
     link.addEventListener("click", () => {
         navbar.classList.remove("active");
     });
+});
+
+const contactForm = document.querySelector("#contact-form");
+
+contactForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    emailjs.sendForm(
+        "service_5cmueu7",
+        "template_4ohacx8",
+        contactForm
+    )
+    .then(() => {
+        alert("Message sent successfully!");
+        contactForm.reset();
+    })
+    .catch((error) => {
+    console.error("EmailJS Status:", error.status);
+    console.error("EmailJS Message:", error.text);
+    alert("Failed to send message. Please check the console.");
+});
 });
